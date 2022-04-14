1a893f167bd56e5414aeb0de02110e5ac626398b
tarannomesty <tarannomesty@gmail.com>
2022-04-14 16:23:13 +0600

    counter app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+
+  const increase = (connection) => {
+    setCount(count + connection) 
+  }
+  const decrease = (connection) => {
+    setCount(count - connection) 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+      <p style={{backgroundColor: "grey",height:"2rem",fontSize:"20px",padding:"10px"}}>The value of the counter is {count}</p>
+      <div className="incrementdecrement">
+        <button onClick= {() => increase(1)} 
+        style={{height:"3rem",margin:"2rem",borderRadius:"10px"}}>
+        Increase By 1
+      </button>
+      <button onClick= {() => increase(5)}
+      style={{height:"3rem",margin:"2rem",borderRadius:"10px"}}>
+        Increase By 5
+      </button>
+      <button onClick= {() => increase(10)}
+      style={{height:"3rem",margin:"2rem",borderRadius:"10px"}}>
+        Increase By 10
+      </button>
+      <button onClick = {() => decrease(1)}
+      style={{height:"3rem",margin:"2rem",borderRadius:"10px"}}>
+        Decrease By 1
+      </button>
+      <button onClick = {() => decrease(5)}
+      style={{height:"3rem",margin:"2rem",borderRadius:"10px"}}>
+        Decrease By 5
+      </button>
+      <button onClick = {() => decrease(10)}
+      style={{height:"3rem",margin:"2rem",borderRadius:"10px"}}>
+        Decrease By 10
+      </button>
+      </div>
     </div>
   );
 }
